a5500b1f4c5dff934225988289d8cdd63f5ff628
Victor <VictorCodrean@users.noreply.github.com>
2021-02-06 10:49:46 -0800

    Populating the restaurant-field with restaurants found

--- assets/js/script-restaurant.js
+++ assets/js/script-restaurant.js
@@ -1,10 +1,7 @@
-var restaurantFieldEl = document.querySelector("#restaurant-field");
 var cuisineName;
 var cuisineId;
 var establishmentId;
-
-restaurantFieldEl.style.display = "none";
-
+var establishmentName;
 var apiKey = "062f1094719e081c0ffc6ab1f3e61902";
 var requestCitiesUrl = "https://developers.zomato.com/api/v2.1/cities?q=";
 
@@ -12,10 +9,74 @@ var requestHeader = new Headers({
     'user-key': apiKey
 });
 
-$("#restaurant-field")
+function displayRestaurants(restaurantsList) {
+    console.log(restaurantsList);
+    var data = restaurantsList;
+    $("#restaurants-found").empty();
+
+    for (var i = 0; i < data.length; i++) {
+        var restaurantField = $("<div>").addClass("card restaurant-field has-background-light");
+        var restaurantMainSection = $("<section>").addClass("columns card-content");
+        var figureBox = $("<figure>").addClass("column");
+        if (data[i].restaurant.featured_image === "") {
+            var imgRestaurant = $("<img>").attr({ src: "assets/images/cat-placeholder.png" });
+        } else {
+            var imgRestaurant = $("<img>").attr({ src: data[i].restaurant.featured_image, alt: data[i].restaurant.name });
+        }
+        var locality = $("<figcaption>").text(data[i].restaurant.location.locality);
+        var restaurantNameSection = $("<div>").addClass("column media-content");
+        var restaurantName = $("<p>").addClass("title is-3").text(data[i].restaurant.name);
+        var userRating = $("<p>").addClass("subtitle is-7").text(data[i].restaurant.user_rating.rating_text + " reviews - " + data[i].restaurant.user_rating.aggregate_rating);
+        var userRatingSpan = $("<span>").addClass("icon is-small");
+        var userRatingIcon = $("<i>").attr({ class: "fas fa-star", "aria-hidden": "true" });
+        var reviewsAmount = $("<span>").text(" (" + data[i].restaurant.user_rating.votes + " Reviews" + ")");
+        var restaurantLocation = $("<p>").addClass("subtitle is-6").text("Address: " + data[i].restaurant.location.address);
+        var hoursOfOperation = $("<p>").addClass("subtitle is-7").text("Hours: " + data[i].restaurant.timings);
+
+        var restaurantDetailsSection = $("<section>").addClass("card-content columns");
+        var optionsColumn = $("<div>").addClass("column");
+        var cuisineData = $("<p>").text("Cuisines: " + data[i].restaurant.cuisines)
+        var costForTwo = $("<p>").text("Cost for two: " + data[i].restaurant.average_cost_for_two);
+        var priceRange = $("<p>").text("Price Avg: " + data[i].restaurant.price_range + (" "));
+        var priceRangeSpan = $("<span>").addClass("icon is-small");
+        var priceRangeIcon = $("<i>").attr({ class: "fas fa-hand-holding-usd", "aria-hidden": "true" });
+        var highlightsColumn = $("<div>").addClass("column");
+        var highlightsOfItems = $("<p>").addClass("has-text-centered").text("Highlights");
+        var highlightsData = $("<p>").text(data[i].restaurant.highlights[i]);
+
+        var phoneNumber = $("<p>").text("Phone Number: " + data[i].restaurant.phone_numbers);
+
+
+        $("#restaurants-found").append(restaurantField);
+        restaurantField.append(restaurantMainSection);
+        restaurantMainSection.append(figureBox);
+        figureBox.append(imgRestaurant, locality);
+        restaurantMainSection.append(restaurantNameSection);
+        restaurantNameSection.append(restaurantName, userRating.append(userRatingSpan.append(userRatingIcon), reviewsAmount), restaurantLocation, hoursOfOperation);
+
+        restaurantField.append(restaurantDetailsSection);
+        restaurantDetailsSection.append(optionsColumn);
+        optionsColumn.append(cuisineData, costForTwo, priceRange);
+        priceRange.append(priceRangeSpan);
+        if (data[i].restaurant.price_range === 1) {
+            priceRangeSpan.append(priceRangeIcon);
+        } else if (data[i].restaurant.price_range === 2) {
+            priceRangeSpan.append(priceRangeIcon);
+            priceRangeSpan.append(priceRangeIcon);
+        } else if (data[i].restaurant.price_range === 3) {
+            priceRangeSpan.append(priceRangeIcon);
+            priceRangeSpan.append(priceRangeIcon);
+            priceRangeSpan.append(priceRangeIcon);
+        }
+
+        restaurantDetailsSection.append(highlightsColumn);
+        highlightsColumn.append(highlightsOfItems);
+        highlightsColumn.append(highlightsData);
+    }
+}
+
 function searchRestaurantsByCity(cityId, cuisineId, establishmentId) {
-    console.log(localStorage.getItem("cuisine"))
-    console.log(establishmentId)
+    console.log(establishmentId);
     var requestRestaurantsUrl = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&cuisines=${cuisineId}&establishment_type=${establishmentId}`;
     console.log(requestRestaurantsUrl);
 
@@ -25,17 +86,14 @@ function searchRestaurantsByCity(cityId, cuisineId, establishmentId) {
         return response.json();
     }).then(function (data) {
 
-        
         console.log(data);
-
-
-        $("#restaurant-field").append()
-
+        var restaurantsList = data.restaurants;
+        displayRestaurants(restaurantsList);
     });
 }
 
 function searchEstablishmentId(cityId) {
-    var requestEstablishmentUrl = `https://developers.zomato.com/api/v2.1/establishments?city_id=${cityId}`
+    var requestEstablishmentUrl = `https://developers.zomato.com/api/v2.1/establishments?city_id=${cityId}`;
 
     fetch(requestEstablishmentUrl, {
         headers: requestHeader
@@ -46,22 +104,21 @@ function searchEstablishmentId(cityId) {
 
         for (var i = 0; i < data.establishments.length; i++) {
             if (data.establishments[i].establishment.name == establishmentName) {
-                console.log(data.establishments[i].establishment.name)
+                console.log(data.establishments[i].establishment.name);
 
-                establishmentId = data.establishments[i].establishment.id
-                console.log(establishmentId)
-
-
+                establishmentId = data.establishments[i].establishment.id;
+                console.log(establishmentId);
             }
         }
+        console.log(establishmentId);
         searchRestaurantsByCity(cityId, cuisineId, establishmentId);
     });
 
-    console.log(establishmentId)
+
 }
 
 function searchCuisinesId(cityId) {
-    var requestCuisineUrl = `https://developers.zomato.com/api/v2.1/cuisines?city_id=${cityId}`
+    var requestCuisineUrl = `https://developers.zomato.com/api/v2.1/cuisines?city_id=${cityId}`;
 
     fetch(requestCuisineUrl, {
         headers: requestHeader
@@ -72,22 +129,14 @@ function searchCuisinesId(cityId) {
 
         for (var i = 0; i < data.cuisines.length; i++) {
             if (data.cuisines[i].cuisine.cuisine_name == cuisineName) {
-                cuisineId = data.cuisines[i].cuisine.cuisine_id
-
-                console.log(cuisineId)
-
-                localStorage.setItem("cuisine", cuisineId);
-
-
+                cuisineId = data.cuisines[i].cuisine.cuisine_id;
+                console.log(cuisineId);
             }
-
         }
-        console.log(cuisineId)
     });
 }
 
 function searchCityId(cityName) {
-
     // building the complete api call url by adding the cityName parameter to the base url
     // note the second parameter to the fetch() call, which is an object with a "headers"
     // property, which I set equal to our requestHeader object from earlier
@@ -99,30 +148,19 @@ function searchCityId(cityName) {
         // log the returned data.
         // (Look at the "location_suggestions" property of the returned object)
         console.log(data);
-
         var cityId = data.location_suggestions[0].id;
         console.log(cityId);
         searchCuisinesId(cityId);
-
         searchEstablishmentId(cityId);
-
-
     });
 }
 
-
-
-
 $("#search-button-rest").on("click", function (event) {
     event.preventDefault();
-    restaurantFieldEl.style.display = "block";
-
     var cityNameInput = $("#search-input-city").val();
     var restaurantNameSelect = $("#search-input-rest").val();
-
     var cuisineOptionEl = document.querySelector("#cuisine-option");
     cuisineName = cuisineOptionEl.value;
-
     var establishmentEl = document.querySelector("#establishment-option");
     establishmentName = establishmentEl.value;
 
@@ -131,9 +169,4 @@ $("#search-button-rest").on("click", function (event) {
     console.log(establishmentName);
 
     searchCityId(cityNameInput);
-
-    // getRestaurantsAround(cityNameInput);
-
-
-    // https://javascript.info/var - Example
 });
