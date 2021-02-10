92be3e8157f07494fadad7b2c9b62b2943d8b5bf
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-02-09 23:12:35 -0800

    Finialized first sprint event js, css, html

--- assets/js/script-events.js
+++ assets/js/script-events.js
@@ -16,9 +16,10 @@ var listEventItem = $("li");
 
 //This function populates and displays the data on the browser.
 function eventResults (displayEvents) {
+    $("#events-field").empty();
     for (var i = 0; i < displayEvents.length; i++){
         
-    var eventsField = $("<div>").addClass("card event-field has-background-light");
+    var eventsField = $("<div>").addClass("card event-field has-background-light margin");
     var eventMainSection = $("<section>").addClass("columns card-content");
     var eventImg = $("<figure>").addClass("column");
     // If url is null, then we display an img placeholder.
@@ -27,7 +28,8 @@ function eventResults (displayEvents) {
     } else {
         var imgEvent = $("<img>").attr({ src: displayEvents[i].images[0].url, alt: displayEvents[i].images[0].url });
     }
-    var venue = $("<figcaption>").text(displayEvents[i]._embedded.venues.name);
+    var venue = $("<figcaption>").text(displayEvents[i]._embedded.venues[0].name);
+        console.log(displayEvents[i]._embedded.venues[0].name);
     var eventNameSection =  $("<div>").addClass("column media-content");
     var eventName = $("<p>").addClass("title is-3").text(displayEvents[i].name);
     var eventDateAndTime = $("<p>").addClass("title is-4").text(displayEvents[i].start);
@@ -36,11 +38,10 @@ function eventResults (displayEvents) {
     var eventPriceRangeCurrency = $("<span>").addClass("subtitle is-5").text(displayEvents[i].priceRanges[0].currency);
     var infoColumn = $("<div>").addClass("row card-content");
     var ticketURL = $("<a>").attr("href", displayEvents[i].url).text("Purchase your tickets!")
-    var ticketNote = $("<span>").text("PLEASE NOTE: " + displayEvents[i].pleaseNote);
-    var ticketLimit = $("<p>").text(displayEvents[i].accessibility.ticketLimit);
-    var ageRestrictions = $("<p>").text(displayEvents[i].ageRestrictions.legalAgeEnforced);
-    var accessibility = $("<p>").text(displayEvents[i]._embedded.venues[0].accessibleSeatingDetail);
-    var generalInfo = $("<p>").text(displayEvents[i]._embedded.venues[0].generalInfo);
+    var ticketNote = $("<div>").text("PLEASE NOTE: " + displayEvents[i].pleaseNote);
+    var ticketLimit = $("<div>").text("Ticket limit per purchase: " + displayEvents[i].accessibility.ticketLimit);
+    var accessibility = $("<div>").text(displayEvents[i]._embedded.venues[0].accessibleSeatingDetail);
+    var generalInfo = $("<div>").text(displayEvents[i]._embedded.venues[0].generalInfo);
 
     $("#events-field").append(eventsField);
         eventsField.append(eventMainSection);
@@ -51,7 +52,7 @@ function eventResults (displayEvents) {
         eventNameSection.append(eventPriceRangeMin.append(eventPriceRangeMax, eventPriceRangeCurrency));
         eventNameSection.append(infoColumn);
         infoColumn.append(ticketURL);
-        infoColumn.append(ticketNote.append(ticketLimit, ageRestrictions, accessibility, generalInfo));
+        eventNameSection.append(ticketLimit.append(ticketNote, accessibility, generalInfo));
 
     }
 }
@@ -88,4 +89,3 @@ $("#search-button-event").on("click", function(event){
 // Need to be able to pull every event name (one variable)
 // Make a function to populate the list (append)
 // Ensure that the loop actually stops at 10 per page (console log)
-// 
